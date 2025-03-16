3ebca24fa3f96b58183747add53694a68fa6d09c
Saiha Atiq <saeyha123456@gmail.com>
2025-03-16 13:20:48 +0500

    Add files via upload

---
+++ lab2AI.py
@@ -0,0 +1,139 @@
+# count = 0
+# while(count<3):
+#   count=count+1
+#   print("Malaika")
+
+# Output:
+# Malaika
+# Malaika
+# Malaika
+
+# ---------------------------------------------
+# count=0
+# while(count==0):
+#     print("Hi!") #infinite loop
+
+# -------------------------------------------------
+# t=['Hello',"i'm", "Saiha"]
+# for i in t:
+#     print(i)
+
+# output:
+# Hello
+# i'm
+# Saiha
+
+# ----------------------------------------------------
+
+# s="Geeks"
+# for i in s:
+#     print(i)
+
+# output:
+# G
+# e
+# e
+# k
+# s
+
+# ----------------------------------------------
+# this=["hi","what","is","this"]
+# for i in range(len(this)):
+#     print (this[i])
+
+# output:
+# hi
+# what
+# is
+# this
+
+# --------------------------------------------------------------------
+# COUNTINUE STATEMENT
+# for letter in 'saiha':
+#     if letter=='h':
+#      break
+# print ('Current letter:'),letter # ?? What is wrong with this code, as it is not printing anything?
+
+# ---------------------------------------------------------------------
+# def my_fun():
+#     print("This is mine")
+# my_fun()
+
+# output:
+# This is mine
+
+# -----------------------------------------------------------------------
+# r=3
+# def my_fun(x):
+#     y=x+4
+#     return y
+# print (my_fun(r))
+
+# output:
+# 7
+# ----------------------------------------------------------------
+#Default Parameter Value
+# def my_fun(country="Pakistan"):
+#     print("I am from",country)
+
+# my_fun()
+# my_fun("Norway")
+
+# output:
+# I am from Pakistan
+# I am from Norway
+# -------------------------------------------------------------------
+# def my_fun(fruits):
+#     for x in fruits:
+#         print(x)
+# food=['banana','apple','potato']
+# my_fun(food)
+# output:
+# banana
+# apple
+# potato
+# -------------------------------------------------------------------
+# def my_function(x):
+#  return 5 * x
+# -------------------------------------------------------------------
+# def children(child1,child2,child3):
+#     print("the youngest child is",child3)
+# children(child1="Raiha",child2="Okasha",child3="Saiha")
+
+# output:
+# the youngest child is Saiha
+# -------------------------------------------------------------------
+
+# Classes
+# class Myclass:
+#     x=5
+# p1=Myclass()
+# print(p1.x)
+
+# output:
+# 5
+# -------------------------------------------------------------------
+
+# class person:
+#     def __init__(self,name,age):
+#         self.name=name
+#         self.age=age
+# p=person("Siaha",30)
+# print(p.name,p.age)
+#output:
+#Siaha 30
+
+# -------------------------------------------------------------------
+# class person:
+#     def __init__(self,name,age):
+#         self.name = name
+#         self.age = age
+#     def my_fun(self):
+#         print("Hello my name is", self.name)
+# p=person("Saiha",30)
+# p.my_fun()
+
+# output:
+# Hello my name is Saiha
+    
+# This is the end!!!!!!!!!!!!!!!!!!!!
